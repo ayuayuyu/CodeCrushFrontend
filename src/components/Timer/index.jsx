8cce23ac8c18f0efaf47a74b6dc88d89b065df80
kinako1415 <Owata09@outlook.jp>
2024-12-02 22:36:10 +0900

    timer関数の実装

---
+++ src/components/Timer/index.jsx
@@ -0,0 +1,79 @@
+import { useCallback, useEffect, useState } from "react";
+import { useTimer } from "react-timer-hook";
+
+export const Timer = ({ expiryTimestamp }) => {
+  const [countTime, setCountTime] = useState("timerHigh");
+
+  const { seconds, minutes, isRunning, start, pause, resume, restart } =
+    useTimer({
+      expiryTimestamp,
+      onExpire: () => console.warn("onExpire called"),
+      autoStart: false,
+    });
+
+  const updateCountTime = useCallback(() => {
+    if (minutes < 1 && seconds < 20) {
+      setCountTime("timerLow");
+    } else if (minutes < 1) {
+      setCountTime("timerMedium");
+    } else {
+      setCountTime("timerHigh");
+    }
+  }, [minutes, seconds]);
+
+  useEffect(() => {
+    updateCountTime();
+  }, [updateCountTime]);
+
+  const timerStyles = {
+    timerHigh: {
+      backgroundColor: "#E8FDE7",
+      color: "#2ADA6E",
+      border: "3px solid #2ADA6E",
+    },
+    timerMedium: {
+      backgroundColor: "#FFF6D6",
+      color: "#FFC80B",
+      border: "3px solid #FFC80B",
+    },
+    timerLow: {
+      backgroundColor: "#FFE3E3",
+      color: "#E54671",
+      border: "3px solid #E54671",
+    },
+  };
+
+  return (
+    <div>
+      <div>{isRunning ? "running" : "Not running"}</div>
+      <div
+        style={{
+          ...timerStyles[countTime],
+          borderRadius: "30px",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          fontSize: "42px",
+          fontWeight: "bold",
+          flexGrow: "1",
+        }}
+      >
+        <div>
+          <span style={{ fontSize: "14px" }}>残り時間</span>
+          <span>{minutes}</span>:
+          <span>{seconds < 10 ? "0" + seconds : "" + seconds}</span>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        <button onClick={start}>start</button>
+        <button onClick={pause}>pause</button>
+        <button onClick={resume}>resume</button>
+        <button onClick={restart}>restart</button>
+      </div>
+    </div>
+  );
+};
--- src/pages/Title/index.jsx
+++ src/pages/Title/index.jsx
@@ -1,6 +1,11 @@
+import { Timer } from "../../components/Timer";
+
 export const Title = () => {
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 62);
   return (
     <>
+      <Timer expiryTimestamp={time} />
       <p>Title</p>
     </>
   );
