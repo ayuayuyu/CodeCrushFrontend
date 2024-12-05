d5c96fbbcd36990e5c42a3460db1806303975f51
kinako1415 <Owata09@outlook.jp>
2024-12-05 13:19:34 +0900

    修正

--- src/pages/Read/index.jsx
+++ src/pages/Read/index.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Timer } from "../../components/Timer";
-import { MenuList } from "../../components/MenuList";
 
 export const Read = () => {
   const [isFinish, serIsFinish] = useState(false);
@@ -8,7 +7,6 @@ export const Read = () => {
     <>
       <Timer setIsFinish={serIsFinish} initialTime={61} />
       <p>{isFinish ? "true" : "false"}</p>
-      <MenuList />
     </>
   );
 };
