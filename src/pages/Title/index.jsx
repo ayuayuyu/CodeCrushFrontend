ea6a325c4dfc6fe5f788ba3190a8a5057dc9913e
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2024-12-06 00:46:33 +0900

    add:"タイトル画面の作成"

--- src/pages/Title/index.jsx
+++ src/pages/Title/index.jsx
@@ -1,7 +1,18 @@
+import "./index.css";
+import { useNavigate } from "react-router-dom";
+
 export const Title = () => {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <p>Title</p>
-    </>
+    <div className="title-container" onClick={() => navigate("/read")}>
+      <div className="titleBox">
+        <div className="logo">
+          <p>ロゴ</p>
+        </div>
+        <p className="main-title">CodeCrush</p>
+      </div>
+      <p className="subtitle">read & create</p>
+    </div>
   );
 };
