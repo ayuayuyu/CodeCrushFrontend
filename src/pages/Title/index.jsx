057135b4f9690fbc6e83d9e5e6ff6381244d523e
ayu <umaminokiami@gmail.com>
2024-12-06 16:09:01 +0900

    タイトル画面の修正

--- src/pages/Title/index.jsx
+++ src/pages/Title/index.jsx
@@ -7,16 +7,14 @@ export const Title = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="title-container" onClick={() => navigate("/read")}>
-      <div className="titleBox">
-        <div className="logo">
-          <Logo/>
-        </div>
-        <p className="main-title">CodeCrush</p>
-      </div>
-      <p className="subtitle">read & create</p>
-    </div>
     <>
+      <div className="title-container" onClick={() => navigate("/read")}>
+        <div className="titleBox">
+          <Logo />
+          <p className="main-title">CodeCrush</p>
+        </div>
+        <p className="subtitle">read & create</p>
+      </div>
       <p>Title</p>
       <div style={{ display: "flex", gap: 20 }}>
         <Logo />
