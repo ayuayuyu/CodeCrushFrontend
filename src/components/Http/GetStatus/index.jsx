3521ec4f20889ca21d7b4e45447edae0ea04ae17
kinako1415 <Owata09@outlook.jp>
2024-12-08 04:53:46 +0900

    issueの解消

--- src/components/Http/GetStatus/index.jsx
+++ src/components/Http/GetStatus/index.jsx
@@ -1,16 +1,14 @@
 import { useEffect, useState } from "react";
 const apiUrl = import.meta.env.VITE_API_URL_2;
 
-export const GetStatus = ({ roomId }) => {
-  const [state, setState] = useState("");
-
+export const GetStatus = ({ roomId, setState }) => {
   useEffect(() => {
     // 無名関数で非同期処理をラップ
     const fetchData = async () => {
       //エンドポイント
       const url = `${apiUrl}/status/${roomId}`;
       //デバック用
-      console.log(`GetStatus : roomId: ${roomId} status: ${status}`);
+      console.log(`GetStatus : roomId: ${roomId}`);
 
       try {
         const response = await fetch(url, {
@@ -24,13 +22,12 @@ export const GetStatus = ({ roomId }) => {
 
         const data = await response.json();
         console.log("Success,status:", data.status);
-        // setState(data);
+        setState(data.status);
       } catch (error) {
         console.error("Error:", error);
-        setState({ error: error.message });
       }
     };
 
     fetchData();
-  }, [roomId, status]); // `status` も依存関係に追加
+  }, [roomId]); // `status` も依存関係に追加
 };
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { Code } from "../Code";
 import { Output } from "../Output/index";
 import { Player } from "../Player";
@@ -7,11 +7,24 @@ import { DocumentButton } from "./../CodeDocument/Button";
 import "./index.css";
 import { CodeDocument } from "../CodeDocument/Docment";
 import { CodeContext } from "../../contexts/CodeContext";
+import { RoomIdContext } from "../../contexts/RoomId";
+import { GetStatus } from "../Http/GetStatus";
 
 export const Main = ({ editFlag }) => {
   const { code, setCode } = useContext(CodeContext);
   const [outputArray, setOutputArray] = useState([]);
   const [isDocumentOpen, setIsDocumentOpen] = useState(false);
+  const { roomId, setRoomId } = useContext(RoomIdContext);
+  const [state, setState] = useState("");
+  GetStatus({ roomId, setState });
+  console.log(`mainStatus: ${state}`);
+
+  // const phaseStatus = "answer";
+  useEffect(() => {
+    if (state === "answer") {
+      setIsDocumentOpen(true);
+    }
+  }, [state]);
 
   return (
     <div className="main-container">
@@ -25,8 +38,14 @@ export const Main = ({ editFlag }) => {
         <div className={`main-inner ${isDocumentOpen ? "open" : ""}`}>
           <div className="main-header">
             <Player pattern={1} />
-            <Run code={code} setOutputArray={setOutputArray} />
-            <DocumentButton setIsDocumentOpen={setIsDocumentOpen} />
+            {state !== "answer" ? (
+              <>
+                <Run code={code} setOutputArray={setOutputArray} />
+                <DocumentButton setIsDocumentOpen={setIsDocumentOpen} />
+              </>
+            ) : (
+              <></>
+            )}
           </div>
           <div
             style={{
@@ -39,7 +58,25 @@ export const Main = ({ editFlag }) => {
           />
           <Code isCanEdit={editFlag} />
         </div>
-        {isDocumentOpen ? <CodeDocument /> : <div />}
+        {state === "answer" ? (
+          <div className={`main-inner ${isDocumentOpen ? "answer-open" : ""}`}>
+            <div className="main-header">
+              <Player pattern={3} />
+            </div>
+            <div
+              style={{
+                height: "2px",
+                width: "99.8%",
+                margin: "0 auto 6px auto",
+                borderRadius: "10px",
+                backgroundColor: "#DBE2E7",
+              }}
+            />
+            <Code isCanEdit={editFlag} />
+          </div>
+        ) : (
+          <> {isDocumentOpen ? <CodeDocument /> : <></>}</>
+        )}
       </div>
       <Output outputArray={outputArray} />
     </div>
