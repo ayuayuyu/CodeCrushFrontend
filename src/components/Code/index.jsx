7d8aaa3ebf48e40f8a42c36483306f7e91f74efc
kinako1415 <Owata09@outlook.jp>
2024-12-07 19:50:29 +0900

    CodeDocumentコンポーネントの作成

--- src/components/Code/index.jsx
+++ src/components/Code/index.jsx
@@ -1,6 +1,7 @@
 import { useCallback, useState, useEffect } from "react";
 import ReactCodeMirror, { EditorView } from "@uiw/react-codemirror";
 import { cppLanguage } from "@codemirror/lang-cpp";
+
 export const Code = ({ code, setCode }) => {
   //const [code, setCode] = useState(""); //コードエディタ上に表示するコードを格納する
   const [canEdit, setCanEdit] = useState(false); //編集可能にするかどうか
@@ -28,30 +29,32 @@ export const Code = ({ code, setCode }) => {
   }, []);
 
   return (
-    <ReactCodeMirror
-      value={code}
-      onChange={onChangeCode}
-      extensions={[
-        cppLanguage,
-        EditorView.theme({
-          ".cm-scroller": {
-            minHeight: "45vh",
-          },
-          ".cm-gutters": {
-            backgroundColor: "#FFFFFF",
-            color: "B2BBC5",
-          },
-        }),
-      ]}
-      readOnly={canEdit} //編集可否の設定(true:可能,false:不可能)
-      style={{
-        //width: "65%",
-        height: "45vh",
-        overflowY: "auto",
-        fontSize: "1rem",
-        color: "black",
-        border: "none",
-      }}
-    />
+    <div>
+      <ReactCodeMirror
+        value={code}
+        onChange={onChangeCode}
+        extensions={[
+          cppLanguage,
+          EditorView.theme({
+            ".cm-scroller": {
+              overflowY: "auto",
+              maxHeight: "360px",
+            },
+            ".cm-gutters": {
+              backgroundColor: "#FFFFFF",
+              color: "B2BBC5",
+            },
+          }),
+        ]}
+        readOnly={canEdit} //編集可否の設定(true:可能,false:不可能)
+        style={{
+          width: "100%",
+          // height: "45vh",
+          fontSize: "1rem",
+          color: "black",
+          border: "none",
+        }}
+      />
+    </div>
   );
 };
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,30 +3,36 @@ import { Code } from "../Code";
 import { Output } from "../Output/index";
 import { Player } from "../Player";
 import { Run } from "../Run";
+import { DocumentButton } from "./../CodeDocument/Button";
 import "./index.css";
+import { CodeDocument } from "../CodeDocument/Docment";
 
 export const Main = () => {
   const [code, setCode] = useState("");
   const [outputArray, setOutputArray] = useState([]);
+  const [isDocumentOpen, setIsDocumentOpen] = useState(false);
 
   return (
     <div className="main-container">
-      <div className="main-inner">
-        <div className="main-header">
-          <Player pattern={1} />
-          <Run code={code} setOutputArray={setOutputArray} />
-          仕様書
+      <div style={{ display: "flex", gap: isDocumentOpen ? "22px" : 0 }}>
+        <div className={`main-inner ${isDocumentOpen ? "open" : ""}`}>
+          <div className="main-header">
+            <Player pattern={1} />
+            <Run code={code} setOutputArray={setOutputArray} />
+            <DocumentButton setIsDocumentOpen={setIsDocumentOpen} />
+          </div>
+          <div
+            style={{
+              height: "2px",
+              width: "99.8%",
+              margin: "0 auto 6px auto",
+              borderRadius: "10px",
+              backgroundColor: "#DBE2E7",
+            }}
+          />
+          <Code code={code} setCode={setCode} />
         </div>
-        <div
-          style={{
-            height: "2px",
-            width: "99.8%",
-            margin: "0 auto 6px auto",
-            borderRadius: "10px",
-            backgroundColor: "#DBE2E7",
-          }}
-        />
-        <Code code={code} setCode={setCode} />
+        {isDocumentOpen ? <CodeDocument /> : <div />}
       </div>
       <Output outputArray={outputArray} />
     </div>
