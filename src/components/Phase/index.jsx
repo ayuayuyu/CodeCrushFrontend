a86a3dd36ba6087f94bbf8fc1b21cd5ef1061009
ryoo-ma638 <donoue63ryoma@gmail.com>
2024-12-07 16:57:19 +0900

    Phaseの修正

--- src/components/Phase/index.jsx
+++ src/components/Phase/index.jsx
@@ -1,19 +1,64 @@
-import { Create } from "../../components/Phase/Icons/Create";
-import { Book } from "./../../components/Phase/Icons/Book";
-import { Delete } from "./../../components/Phase/Icons/Delete";
-import { Check } from "../../components/Phase/Icons/Check";
+import "../Phase/index.css";
+import { Book } from "./Icons/Book";
+import { Delete } from "./Icons/Delete";
+import { Create } from "./Icons/Create";
+import { Check } from "./Icons/Check";
 import { useState } from "react";
 import "./index.css";
 
 export const Phase = () => {
-  const [phase, setPhase] = useState("explanation");
-  //0,1,2,3でフェーズの管理をする
+  const [phase, setPhase] = useState("result");
+  //フェーズの管理をする
   return (
-    <div style={{ display: "flex", gap: "10px" }}>
-      {phase === "explanation" ? <Book isState={1} /> : <Book isState={0} />}
-      {phase === "crush" ? <Delete isState={1} /> : <Delete isState={0} />}
-      {phase === "fix" ? <Create isState={1} /> : <Create isState={0} />}
-      {phase === "result" ? <Check isState={1} /> : <Check isState={0} />}
+    <div className="phase">
+      <p className="phase-icon">
+        {phase === "explanation" ? <Book isState={1} /> : <Book isState={0} />}
+        {phase === "crush" ? <Delete isState={1} /> : <Delete isState={0} />}
+        {phase === "fix" ? <Create isState={1} /> : <Create isState={0} />}
+        {phase === "result" ? <Check isState={1} /> : <Check isState={0} />}
+      </p>
+      {phase === "explanation" ? (
+        <div>
+          <div className="phase-target">
+            <p className="phase-target-word">目標１</p>
+          </div>
+          <p className="phase-word">コードを読み込もう！！</p>
+        </div>
+      ) : (
+        <div></div>
+      )}
+      {phase === "crush" ? (
+        <div>
+          <div className="phase-target">
+            <p className="phase-target-word">目標2</p>
+          </div>
+          <p className="phase-word">コードを消して妨害しよう！</p>
+        </div>
+      ) : (
+        <div></div>
+      )}
+
+      {phase === "fix" ? (
+        <div>
+          <div className="phase-target">
+            <p className="phase-target-word">目標3</p>
+          </div>
+          <p className="phase-word">コードを修復しよう！！</p>
+        </div>
+      ) : (
+        <div></div>
+      )}
+
+      {phase === "result" ? (
+        <div>
+          <div className="phase-target">
+            <p className="phase-target-word">目標4</p>
+          </div>
+          <p className="phase-word">答え合わせ！！</p>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
